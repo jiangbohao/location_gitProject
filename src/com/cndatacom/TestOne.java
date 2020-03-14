4d5b5d400c39416ded49cb1d8eb04607fd0a4d0b
jbh <784041285@qq.com>
2020-03-14 15:16:01 +0800

    提交到本地版本库

--- src/com/cndatacom/TestOne.java
+++ src/com/cndatacom/TestOne.java
@@ -8,6 +8,12 @@ package com.cndatacom;
 public class TestOne {
 
     public static void main(String[] args) {
+
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
         System.out.println("hello git");
     }
 }
